149fe34d322fe4b7c883e4e54d89ed80b074048d
Madjid Marwan <madjidmarwan2@gmail.com>
2022-06-16 22:06:31 +0700

    COMMIT 3

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -6,15 +6,15 @@ public class Driver {
 		
 		Counter counter = new Counter();
 		
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Current count2:" + counter.getCount());
 		
 		counter.increment();
 		
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Current count2:" + counter.getCount());
 		
 		counter.decrement();
 		
-		System.out.println("Current count:" + counter.getCount());
+		System.out.println("Current count2:" + counter.getCount());
 		
 	}
 
